43d1b1361c57ae386d456d670ae4dc980db7b818
Jose Soto <jsoto7087@gmail.com>
2022-04-29 09:22:21 -0400

    commit tree 70abf72b5cff2d412817408850a8f48ba26258fc

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const mainMarkUp = document.getElementById('main');
+mainMarkUp.remove();
+
+const newHeader = document.createElement("h1");
+// h1.classList.add("victory")
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Jose is the champion"
